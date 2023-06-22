e560fa4df84b6d08482b31bd9c69763c9e2ae51d
SKorchinskiy <ddphyk@gmail.com>
2023-06-23 02:38:02 +0300

    + changes

--- src/javascript/services/fightersService.js
+++ src/javascript/services/fightersService.js
@@ -3,6 +3,8 @@ import callApi from '../helpers/apiHelper';
 class FighterService {
     #endpoint = 'fighters.json';
 
+    #details = 'details/fighter/';
+
     async getFighters() {
         try {
             const apiResult = await callApi(this.#endpoint);
@@ -13,6 +15,9 @@ class FighterService {
     }
 
     async getFighterDetails(id) {
+        const mountedUrl = `${this.#details}${id}.json`;
+        const fighterDetails = await callApi(mountedUrl);
+        return fighterDetails;
         // todo: implement this method
         // endpoint - `details/fighter/${id}.json`;
     }
